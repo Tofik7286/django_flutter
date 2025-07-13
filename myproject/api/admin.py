fec036dfb03cd6afdd00c95e4f810758d211aa4c
tofik <tofik.imscit21@gmail.com>
2025-07-13 11:49:59 +0530

    Starting of project

--- myproject/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-admin.site.register(UserProfile)
-
-
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email']
